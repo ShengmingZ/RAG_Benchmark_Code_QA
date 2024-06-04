d45b60842e64068c9143d2f5302621dc67891a19
赵盛铭 <zhaosm1729@gmail.com>
2024-06-04 10:52:15 -0600

    up

--- generator/eval.py
+++ generator/eval.py
@@ -17,7 +17,12 @@ args = config()
 if args.analysis_type == "retrieval_recall":
     # ret_acc_list = [1, 0.8, 0.6, 0.4, 0.2, 0]
     ret_acc_list = [1]
+    result_list = list()
     for ret_acc in ret_acc_list:
         cmd = f'python --model {args.model} --dataset {args.dataset} --retriever {args.retriever} --analysis_type {args.analysis_type} --ret_acc {ret_acc}'
-        subprocess.check_output(cmd, shell=True)
-        print(f'done ret_acc {ret_acc}')
+        proc = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (output, error) = proc.communicate()
+        passk = output.decode().split('\n')[-2]
+        result_list.append(passk)
+    for ret_acc, result in zip(ret_acc_list, result_list):
+        print('ret_acc: ', ret_acc, result)
