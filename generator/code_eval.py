6c0d725cb957851375e9cfbc20f10a4aadcb5b45
赵盛铭 <zhaosm1729@gmail.com>
2024-06-05 23:04:13 -0600

    update

--- generator/code_eval.py
+++ generator/code_eval.py
@@ -16,9 +16,17 @@ def process_gene_results(args, outputs):
     if args.dataset == 'conala' and args.model == 'codellama-13b-instruct':
         preds = []
         for output in outputs:
-            pred = output.replace(' <code>', '')
-            lines = pred.split(' ```\n')
-            if len(lines) > 1: pred = lines[1].split('\n```')[0]
+            if output.startswith(' '): output = output[1:]
+            pred = output.replace('<code>', '')
+            if '`' in pred:
+                lines = pred.split('\n')
+                try:
+                    _pred = lines[1]
+                    pred = _pred
+                except: ...
+            # if len(lines) > 1: pred = lines[1].split('\n```')[0]
+            # lines = pred.split('`')
+            # if len(lines) > 1: pred = lines[1].split('`')[0]
             preds.append(pred)
 
     return preds
@@ -36,10 +44,14 @@ def conala_eval(args):
 
 
 if __name__ == '__main__':
-    # in_program_call = '--model codellama-13b-instruct --dataset conala --retriever openai-embedding --analysis_type retrieval_recall --ret_acc 0.8'
-    in_program_call = None
+    in_program_call = '--model codellama-13b-instruct --dataset conala --retriever openai-embedding --analysis_type retrieval_recall --ret_acc 0.8'
+    # in_program_call = None
     args = generate_config(in_program_call)
 
     passk = conala_eval(args)
 
-    # process_gene_results(args)
+    # gene_results = json.load(open(args.save_file, 'r'))
+    # for result in gene_results:
+    #     # print(result['outputs'])
+    #     outputs = process_gene_results(args, result['outputs'])
+    #     print(outputs)
