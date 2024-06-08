5a8d7185f51537c88579a8e5fb9e4bd06c6e99eb
赵盛铭 <zhaosm1729@gmail.com>
2024-06-08 12:37:39 -0600

    update

--- generator/generate.py
+++ generator/generate.py
@@ -141,7 +141,7 @@ if __name__ == '__main__':
     # gene_conala.gene_response()
 
     in_program_call = None
-    in_program_call = '--model codellama-13b-instruct --dataset DS1000 --retriever BM25 --analysis_type retrieval_recall --ret_acc 1'
+    in_program_call = '--model codellama-13b-instruct --dataset pandas_numpy_eval --retriever openai-embedding --analysis_type retrieval_recall --ret_acc 1'
     args = generate_config(in_program_call)
     generator = Generator(args)
     generator.test_prompt()
