688c2a1db6ef39142562b8c6ac80993aa601aa03
赵盛铭 <zhaosm1729@gmail.com>
2024-07-09 11:39:29 -0600

    up

--- generator/run_model.py
+++ generator/run_model.py
@@ -52,12 +52,19 @@ def chatgpt_batch(prompt_file_for_batch, prompts, model, temperature=0.7, max_to
         for request in requests:
             f.write(json.dumps(request)+'\n')
 
-    batch_input_file = client.files.create(file=open(prompt_file_for_batch, 'rb'), purpose='batch')
-    response = client.batches.create(input_file_id=batch_input_file.id,
-                                     endpoint='/v1/chat/completions',
-                                     completion_window='24h',
-                                     metadata={'description': prompt_file_for_batch})
-    batch_id = response.id
+    # if same metadata exists, then just get results, else create batch
+    batch_id = None
+    batches = client.batches.list()
+    for data in batches.data:
+        if data.metadata['description'] == prompt_file_for_batch:
+            batch_id = data.id
+    if batch_id is None:
+        batch_input_file = client.files.create(file=open(prompt_file_for_batch, 'rb'), purpose='batch')
+        response = client.batches.create(input_file_id=batch_input_file.id,
+                                         endpoint='/v1/chat/completions',
+                                         completion_window='24h',
+                                         metadata={'description': prompt_file_for_batch})
+        batch_id = response.id
     print('batch_id: ', batch_id)
 
     # extract batch results
