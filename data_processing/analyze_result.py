c649b99be22b8ec3c683c1978be3695704d843eb
赵盛铭 <zhaosm1729@gmail.com>
2024-07-18 15:44:26 -0600

    up

--- data_processing/analyze_result.py
+++ data_processing/analyze_result.py
@@ -1,4 +1,5 @@
 import json
+import ast
 import numpy as np
 import sys, platform
 from scipy.spatial.distance import hamming
@@ -10,9 +11,12 @@ elif system == 'Linux':
 sys.path.insert(0, root_path)
 from generator.generate_utils import generate_config
 from dataset_utils.conala_utils import ConalaLoader
+from dataset_utils.pandas_numpy_eval_utils import PandasNumpyEvalLoader
+from dataset_utils.DS1000_utils import DS1000Loader
 
 
-def perplexity_analysis(results):
+
+def calc_perplexity(results):
     perplexity = 0
     for result in results:
         logprobs = result['logprobs'][0]    # todo: only for n=1
@@ -20,21 +24,150 @@ def perplexity_analysis(results):
         perplexity += np.exp(-sum(logprobs)/len(logprobs))
     perplexity /= len(results)
 
-    print(perplexity)
+    print('perplexity: ', perplexity)
     return perplexity
 
 
+def count_semantic_error(dataset, eval_datas):
+    semantic_error_count = 0  # whether use the desired functions
+    if dataset == 'conala':
+        loader = ConalaLoader()
+    elif dataset == 'DS1000':
+        loader = DS1000Loader()
+    elif dataset == 'pandas_numpy_eval':
+        loader = PandasNumpyEvalLoader()
 
-def code_error_type(eval_datas):
+    oracle_docs_in_output_dict = dict()
+    oracle_list = loader.load_oracle_list()
+    output_records = eval_datas['output_records']
+    for qs_id in output_records.keys():
+        oracle_docs_in_output = list()
+        for item in oracle_list:
+            if item['qs_id'] == qs_id: oracle = item
+        oracle_funcs = [item.rsplit('.', 1)[-1] for item in oracle['oracle_docs']]
+        for func in oracle_funcs:
+            if func in output_records[qs_id][0]:
+                oracle_docs_in_output.append(True)
+            else:
+                oracle_docs_in_output.append(False)
+        oracle_docs_in_output_dict[qs_id] = oracle_docs_in_output
+
+    semantic_partial_correct_count = 0
+    semantic_all_correct_count = 0
+    for qs_id in oracle_docs_in_output_dict.keys():
+        if True in oracle_docs_in_output_dict[qs_id]: semantic_partial_correct_count += 1
+        if False not in oracle_docs_in_output_dict[qs_id]: semantic_all_correct_count += 1
+
+    print('semantic partial correct percentage: ', semantic_partial_correct_count/len(output_records))
+    print('semantic correct percentage: ', semantic_all_correct_count/len(output_records))
+
+
+
+def count_syntax_error(dataset, eval_datas):
     syntax_error_count = 0
+    output_records = eval_datas['output_records']
+    programs = list()
+    if dataset == 'conala':
+        loader = ConalaLoader()
+        unittests = json.load(open(loader.unittest_file, 'r'))
+        for qs_id in output_records.keys():
+            unittest = unittests[qs_id]
+            program = f"{unittest['prompt']}{output_records[qs_id][0]}{unittest['suffix']}"
+            programs.append(program)
+    elif dataset == 'pandas_numpy_eval':
+        loader = PandasNumpyEvalLoader()
+        unittests = json.load(open(loader.pandas_eval_file, 'r'))
+        for qs_id in output_records.keys():
+            for item in unittests:
+                if item['task_id'] == qs_id: unittest = item
+            program = unittest['prompt'] + output_records[qs_id][0]
+            programs.append(program)
+    elif dataset == 'DS1000':
+        loader = DS1000Loader()
+        unittests = json.load(open(loader.sampled_data_file, 'r'))
+        for qs_id in output_records.keys():
+            for item in unittests:
+                if item['qs_id'] == qs_id: unittest = item
+            program = unittest['code_context'].replace('[insert]', output_records[qs_id][0])
+            programs.append(program)
+
+    for program in programs:
+        try:
+            ast.parse(program)
+        except:
+            syntax_error_count += 1
+
+    print('Syntax error percentage: ', syntax_error_count / len(output_records))
 
 
 
-def retrieval_vs_eval(eval_datas):
-    # todo: for code, detect if api appears in LLM
-    # todo: for qa: how to detect?
-    retrieval_in_eval_count = 0
-    retrieval_not_in_eval_count = 0
+def retrieval_consistency(eval_datas):
+    # todo: for qa: detect if answer in the corpus
+    retrieval_in_output_count = 0
+    retrieval_not_in_output_count = 0
+    retrieval_consistency = 0
+    output_records = eval_datas['output_records']
+    retrieval_records = eval_datas['retrieval_records']
+    retrieval_in_output_dict = dict()
+
+    for key in retrieval_records.keys():
+        retrieval_in_output = list()
+        retrieved_funcs = [item.rsplit('.', 1)[-1] for item in retrieval_records[key]]
+        output = output_records[key][0]     # todo: now only for n=1
+        for func in retrieved_funcs:
+            if func in output:
+                retrieval_in_output.append(True)
+            else:
+                retrieval_in_output.append(False)
+        retrieval_in_output_dict[key] = retrieval_in_output
+
+
+    for key in retrieval_in_output_dict.keys():
+        retrieval_consistency += retrieval_in_output_dict[key].count(True) / len(retrieval_in_output_dict[key])
+        if True in retrieval_in_output_dict[key]: retrieval_in_output_count += 1
+        else: retrieval_not_in_output_count += 1
+
+    print('retrieval consistency: ', retrieval_consistency/len(output_records))
+    print('retrieval in output count: ', retrieval_in_output_count)
+    print('retrieval not in output count: ', retrieval_not_in_output_count)
+    return retrieval_in_output_dict
+
+
+def retrieval_consistency_vs_eval(dataset, eval_datas):
+    retrieval_in_output_dict = retrieval_consistency(eval_datas)
+    eval_records = eval_datas['eval_records']
+    if dataset == 'conala':
+        for key in eval_records.keys():
+            eval_records[key] = eval_records[key]['passed']
+    elif dataset == 'DS1000':
+        for key in eval_records.keys():
+            eval_records[key] = eval_records[key][0]
+    elif dataset == 'pandas_numpy_eval':
+        for key in eval_records.keys():
+            if 'passed' in eval_records[key]: eval_records[key] = True
+            else: eval_records[key] = False
+
+    retrieval_consist_eval_true_count = 0
+    retrieval_not_consist_eval_true_count = 0
+    retrieval_consist_eval_false_count = 0
+    retrieval_not_consist_eval_false_count = 0
+    for key in retrieval_in_output_dict.keys():
+        if True in retrieval_in_output_dict[key]:
+            if eval_records[key] is True:
+                retrieval_consist_eval_true_count += 1
+            else:
+                retrieval_consist_eval_false_count += 1
+        else:
+            if eval_records[key] is True:
+                retrieval_not_consist_eval_true_count += 1
+            else:
+                retrieval_not_consist_eval_false_count += 1
+
+    print('retrieval consist eval True percentage = ', retrieval_consist_eval_true_count / len(eval_records))
+    print('retrieval not consist eval True percentage = ', retrieval_not_consist_eval_true_count / len(eval_records))
+    print('retrieval consist eval False percentage = ', retrieval_consist_eval_false_count / len(eval_records))
+    print('retrieval not consist eval False percentage = ', retrieval_not_consist_eval_false_count / len(eval_records))
+
 
 
 def eval_vs_eval(dataset, eval_datas1, eval_datas2):
@@ -128,25 +261,36 @@ def ret_eval_vs_eval(eval_datas):
 
 
 if __name__ == '__main__':
-    # in_program_call = ('--action eval_pred --model codellama-13b-instruct --temperature 0.0 --dataset conala --retriever openai-embedding '
-    #                    '--analysis_type retrieval_recall --n 1 --ret_acc 1.0')
-    # args = generate_config(in_program_call)
-    # eval_file = args.result_save_file.replace('.json', '_eval.json')
-    # eval_datas = json.load(open(eval_file))
-    #
-    # in_program_call = ('--action eval_pred --model codellama-13b-instruct --temperature 0.0 --dataset conala --retriever openai-embedding '
-    #                    '--analysis_type retrieval_doc_type --n 1 --ret_doc_type none')
-    # args = generate_config(in_program_call)
-    # eval_file = args.result_save_file.replace('.json', '_eval.json')
-    # eval_datas2 = json.load(open(eval_file))
+    """
+    in_program_call = ('--action eval_pred --model codellama-13b-instruct --temperature 0.0 --dataset conala --retriever openai-embedding '
+                       '--analysis_type retrieval_recall --n 1 --ret_acc 1.0')
+    args = generate_config(in_program_call)
+    eval_file = args.result_save_file.replace('.json', '_eval.json')
+    eval_datas = json.load(open(eval_file))
 
-    # ret_eval_vs_eval(eval_datas)
-
-    # eval_vs_eval(args.dataset, eval_datas, eval_datas2)
-
-
-    in_program_call = ('--action eval_pred --model gpt-3.5-turbo-0125 --temperature 0.0 --dataset conala --retriever openai-embedding '
+    in_program_call = ('--action eval_pred --model codellama-13b-instruct --temperature 0.0 --dataset conala --retriever openai-embedding '
                        '--analysis_type retrieval_doc_type --n 1 --ret_doc_type none')
     args = generate_config(in_program_call)
-    results = json.load(open(args.result_save_file))[:1]
-    perplexity_analysis(results)
+    eval_file = args.result_save_file.replace('.json', '_eval.json')
+    eval_datas2 = json.load(open(eval_file))
+
+    ret_eval_vs_eval(eval_datas)
+
+    eval_vs_eval(args.dataset, eval_datas, eval_datas2)
+    """
+
+    in_program_call = (
+        '--action eval_pred --model codellama-13b-instruct --temperature 0.0 --dataset conala --retriever BM25 '
+        '--analysis_type retrieval_doc_selection --n 1 --doc_selection_type top_5')
+    args = generate_config(in_program_call)
+    results = json.load(open(args.result_save_file))
+    eval_file = args.result_save_file.replace('.json', '_eval.json')
+    eval_datas = json.load(open(eval_file))
+
+    calc_perplexity(results)
+
+    retrieval_consistency(eval_datas)
+    # retrieval_consistency_vs_eval(args.dataset, eval_datas)
+
+    count_syntax_error(args.dataset, eval_datas)
+    count_semantic_error(args.dataset, eval_datas)
