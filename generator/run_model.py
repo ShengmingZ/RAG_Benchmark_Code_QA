55f398f2aab0818e50319787b5c4430d33a7aab6
赵盛铭 <zhaosm1729@gmail.com>
2024-08-07 17:57:14 -0600

    up

--- generator/run_model.py
+++ generator/run_model.py
@@ -303,12 +303,15 @@ def run_model_for_flare(questions, model, dataset, temperature=0, max_tokens=500
 
                 # check if each new sent needs retrieve, update stop_list, output_list, logprobs_list
                 sents, sents_logprobs = split_sents_and_logprobs(output_tokens_this_round, logprobs_this_round) # split output and logprobs to each sentences
+                print(sents)
+                print(sents_logprobs)
                 if retrieve_times_list[idx] > 1:    # 1 means first retrieval using question, after that, each retrieval would make sure at least one more sentence is generated
                     output_list[idx] += sents[0]; logprobs_list[idx].extend(sents_logprobs[0])
                     if_retrieve_list[idx] = False
                     sents = sents[1:]; sents_logprobs = sents_logprobs[1:]
                 for sent, logprobs in zip(sents, sents_logprobs):   # for each sentence, if need retrieve, deprecate sentences behind, query retriever
                     ret_flag, new_query = if_retrieve(sent, logprobs)
+                    print(ret_flag, new_query)
                     if ret_flag:
                         if_retrieve_list[idx] = True
                         queries_list[idx].append(new_query)
