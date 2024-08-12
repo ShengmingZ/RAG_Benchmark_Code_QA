91ebf1fe3f42b72f372eddd3a53b4e6bcd960a37
赵盛铭 <zhaosm1729@gmail.com>
2024-08-11 20:58:25 -0600

    up

--- generator/run.py
+++ generator/run.py
@@ -11,7 +11,7 @@ def config():
     parser.add_argument('--analysis_type', type=str, choices=['retrieval_recall', 'retrieval_doc_type', 'retrieval_doc_selection_topk', 'retrieval_doc_selection_pl',
                                                               'prompt_length_0', 'prompt_length_500', 'prompt_length_1000', 'prompt_length_1500', 'prompt_length_2000',
                                                               'prompt_length_4000', 'prompt_length_6000', 'prompt_length_8000', 'prompt_method'])
-    parser.add_argument('--prompt_type', type=str, default=None, choices=['3shot', 'cot', 'self-consistency'])
+    parser.add_argument('--prompt_type', type=str, default=None, choices=['3shot', 'cot', 'self-consistency', 'least_to_most', 'plan_and_solve'])
     parser.add_argument('--action', type=str, choices=['gene_prompts', 'gene_responses', 'eval_pred'])
     parser.add_argument('--n', type=int)
     parser.add_argument('--batch', action='store_true')
