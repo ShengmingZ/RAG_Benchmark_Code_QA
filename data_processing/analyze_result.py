9a967043bd89489736264cf515ddf6604fd526f1
赵盛铭 <zhaosm1729@gmail.com>
2024-08-22 15:12:29 -0600

    up

--- data_processing/analyze_result.py
+++ data_processing/analyze_result.py
@@ -453,8 +453,8 @@ if __name__ == '__main__':
     """compare 2 prediction distributions"""
     datasets = ['NQ', 'TriviaQA', 'hotpotQA']
     datasets = ['conala', 'DS1000', 'pandas_numpy_eval']
-    evals = ['top_5', 'top_10', 'top_15', 'top_20', 'top_40', 'top_60', 'top_80']
-    # evals = ['top_5', 'top_10', 'top_15', 'top_20']
+    # evals = ['top_5', 'top_10', 'top_15', 'top_20', 'top_40', 'top_60', 'top_80']
+    evals = ['top_5', 'top_10', 'top_15', 'top_20']
     if datasets == ['NQ', 'TriviaQA', 'hotpotQA']: model = 'llama2-13b-chat'
     else: model = 'codellama-13b-instruct'
     model = 'gpt-3.5-turbo-0125'
