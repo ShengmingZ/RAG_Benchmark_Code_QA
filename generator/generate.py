77d8bb8a5b07c1d1e9fad613019c6c701e4928b8
赵盛铭 <zhaosm1729@gmail.com>
2024-09-28 13:30:49 -0600

    up

--- generator/generate.py
+++ generator/generate.py
@@ -205,10 +205,10 @@ class Generator:
         if os.path.exists(self.result_save_file):
             print(f'generation results exists for {self.result_save_file}')
             return
-
+        print(self.result_save_file)
         if self.prompt_type == 'ir-cot' or self.prompt_type == 'flare':     # multi retrieval framework
             if self.prompt_type == 'ir-cot':
-                self.qs_list = self.qs_list[15:16]
+                self.qs_list = self.qs_list[17:18]
                 output_list, logprobs_list, ret_doc_keys_list, prompts_list, input_tokens_list, output_tokens_list, retrieve_times_list, queries_list = (
                     run_model_for_ir_cot(questions=[qs['question'] for qs in self.qs_list], model=self.model, dataset=self.dataset,
                                          temperature=self.temperature, max_tokens=self.max_tokens, n=self.n, stop=self.stop))
