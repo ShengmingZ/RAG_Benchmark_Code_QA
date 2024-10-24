95b13b8631a8c82031f0ba83313fe23041d76c25
赵盛铭 <zhaosm1729@gmail.com>
2024-10-23 23:22:06 -0600

    up

--- generator/pred_eval.py
+++ generator/pred_eval.py
@@ -270,8 +270,8 @@ def pred_eval(args, if_eval_retrieval=False, if_calc_perplexity=True, if_code_an
             _gene_results.append(dict(qs_id=result['qs_id'], outputs=outputs))
             output_records[result['qs_id']] = outputs
             retrieval_records[result['qs_id']] = result['ret_docs']
-        # scores, eval_records = loader.eval_passk(_gene_results, k_list=k_list)
-        scores, eval_records = dict(), dict()
+        scores, eval_records = loader.eval_passk(_gene_results, k_list=k_list)
+        # scores, eval_records = dict(), dict()
 
     elif args.dataset == 'pandas_numpy_eval':
         loader = PandasNumpyEvalLoader()
@@ -286,6 +286,7 @@ def pred_eval(args, if_eval_retrieval=False, if_calc_perplexity=True, if_code_an
             output_records[result['qs_id']] = outputs
             retrieval_records[result['qs_id']] = result['ret_docs']
         scores, eval_records = loader.eval_passk(_gene_results, k_list=k_list)
+        # scores, eval_records = dict(), dict()
 
     elif args.dataset == 'NQ' or args.dataset == 'TriviaQA':
         loader = NQTriviaQAUtils(args.dataset)
