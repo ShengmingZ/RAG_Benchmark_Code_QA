f420ac86421f3834c3e2b6816099d7df42de552e
赵盛铭 <zhaosm1729@gmail.com>
2025-06-10 12:27:13 +0800

    add eval

---
+++ expertiments/Eval.py
@@ -0,0 +1,16 @@
+import argparse
+from generator.pred_eval import pred_eval_new
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset', required=True, help='Dataset (conala, DS1000)')
+    parser.add_argument('--model', required=True, help='Model (openai-new, claude)')
+    parser.add_argument('--mode', required=True, choices=['single', 'oracle', 'both'])
+
+    args = parser.parse_args()
+
+    result_path = f'../data/{args.dataset}/new_results/single_{args.model}.json'
+
+    pred_eval_new(dataset=args.dataset, result_path=result_path)
+
