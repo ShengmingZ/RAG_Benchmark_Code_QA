c8842740280799fdf065027f81fb1a5d22570f2f
赵盛铭 <zhaosm1729@gmail.com>
2025-06-10 13:23:26 +0800

    update

--- expertiments/Eval.py
+++ expertiments/Eval.py
@@ -12,6 +12,13 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
+    if args.model == 'openai-new': args.model = 'gpt-4o-mini'
+    elif args.model == 'openai-old': args.model = 'gpt-3.5-turbo-0125'
+    elif args.model == 'llama-new': args.model = 'meta-llama/Llama-3.1-8B-Instruct'
+    elif args.model == 'llama-old-code': args.model = 'codellama/CodeLlama-13b-Instruct-hf'
+    elif args.model == 'llama-old-qa': args.model = 'meta-llama/Llama-2-13b-chat-hf'
+    else: raise Exception('unknown model')
+
     result_path = f'../data/{args.dataset}/new_results/single_{args.model}.json'
 
     pred_eval_new(dataset=args.dataset, result_path=result_path)
