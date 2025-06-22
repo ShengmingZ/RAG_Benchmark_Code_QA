717a1dcc59aec7757d82f10d7b8b001125c4e506
ShengmingZ <you@example.com>
2025-06-22 09:47:26 +0000

    update parsing

--- expertiments/script.sh
+++ expertiments/script.sh
@@ -1,50 +1,57 @@
 # script for LLAMA2 + CODE dataset + Single Oracle analyze
 
-python RunOracleSingle.py --dataset conala --model llama-old-code --mode single &
-python RunOracleSingle.py --dataset conala --model llama-old-code --mode oracle &
+DATASET='conala'
+MODEL='openai-old'
+
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode single &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode oracle &
 wait
 
-python RunOracleSingle.py --dataset conala --model llama-old-code --mode recall --recall 0.0 &
-python RunOracleSingle.py --dataset conala --model llama-old-code --mode recall --recall 0.2 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.0 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.2 &
 wait
 
-python RunOracleSingle.py --dataset conala --model llama-old-code --mode recall --recall 0.4 &
-python RunOracleSingle.py --dataset conala --model llama-old-code --mode recall --recall 0.6 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.4 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.6 &
 wait
 
-python RunOracleSingle.py --dataset conala --model llama-old-code --mode recall --recall 0.8 &
-wait
-
-
-
-python RunOracleSingle.py --dataset DS1000 --model llama-old-code --mode single &
-python RunOracleSingle.py --dataset DS1000 --model llama-old-code --mode oracle &
-wait
-
-python RunOracleSingle.py --dataset DS1000 --model llama-old-code --mode recall --recall 0.0 &
-python RunOracleSingle.py --dataset DS1000 --model llama-old-code --mode recall --recall 0.2 &
-wait
-
-python RunOracleSingle.py --dataset DS1000 --model llama-old-code --mode recall --recall 0.4 &
-python RunOracleSingle.py --dataset DS1000 --model llama-old-code --mode recall --recall 0.6 &
-wait
-
-python RunOracleSingle.py --dataset DS1000 --model llama-old-code --mode recall --recall 0.8 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.8 &
 wait
 
 
 
-python RunOracleSingle.py --dataset pandas_numpy_eval --model llama-old-code --mode single &
-python RunOracleSingle.py --dataset pandas_numpy_eval --model llama-old-code --mode oracle &
+DATASET='DS1000'
+
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode single &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode oracle &
 wait
 
-python RunOracleSingle.py --dataset pandas_numpy_eval --model llama-old-code --mode recall --recall 0.0 &
-python RunOracleSingle.py --dataset pandas_numpy_eval --model llama-old-code --mode recall --recall 0.2 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.0 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.2 &
 wait
 
-python RunOracleSingle.py --dataset pandas_numpy_eval --model llama-old-code --mode recall --recall 0.4 &
-python RunOracleSingle.py --dataset pandas_numpy_eval --model llama-old-code --mode recall --recall 0.6 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.4 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.6 &
 wait
 
-python RunOracleSingle.py --dataset pandas_numpy_eval --model llama-old-code --mode recall --recall 0.8 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.8 &
+wait
+
+
+
+DATASET='pandas_numpy_eval'
+
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode single &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode oracle &
+wait
+
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.0 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.2 &
+wait
+
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.4 &
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.6 &
+wait
+
+python RunOracleSingle.py --dataset $DATASET --model $MODEL --mode recall --recall 0.8 &
 wait
